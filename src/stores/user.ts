fcb91a21b36694420a2fe1a8e0d1345c9ff115fd
kuzen <xiaokundaa@gmail.com>
2024-07-23 22:41:58 +0800

    chore: 消除回到顶部bug

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { ref, computed } from 'vue'
+import { ref, } from 'vue'
 // import { useStorage } from '@vueuse/core'
 
 // const storage = useStorage('bili-token', '')
