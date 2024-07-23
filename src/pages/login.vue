cbe9f5cf285b9d3ecf58179f985a5a4e8296ef2f
kuzen <xiaokundaa@gmail.com>
2024-07-23 12:05:35 +0800

    chore: login页面的表单

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,7 +1,40 @@
 <template>
-  <h1>test</h1>
+  <div class="login-container">
+    <div class="login-form">
+        <a-form
+        :model="formState"
+        >
+            <a-form-item>
+                <a-input></a-input>
+            </a-form-item>
+        </a-form>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref,reactive,computed } from 'vue'
+interface FormState {
+    email: string;
+}
 
-<style lang="scss"></style>
+
+</script>
+
+<style lang="scss">
+.login-container {
+  position: relative;
+  height: 100vh;
+  min-width: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #eee;
+  .login-form {
+    padding: 20px;
+    background-color:rgba(255,255,255,0.8) ;
+    border-radius:20px;
+    z-index: 10;
+  }
+}
+</style>
