303f85401468c8a6d38750bc25a81c96bbde851e
kuzen <xiaokundaa@gmail.com>
2024-07-24 14:48:21 +0800

    chore: 去除不需要的组件

--- src/pages/user.vue
+++ src/pages/user.vue
@@ -78,7 +78,7 @@ import { use } from 'echarts/core'
 import { CanvasRenderer } from 'echarts/renderers'
 import { PieChart } from 'echarts/charts'
 import { TitleComponent, TooltipComponent, LegendComponent } from 'echarts/components'
-import VChart, { THEME_KEY } from 'vue-echarts'
+
 
 use([CanvasRenderer, PieChart, TitleComponent, TooltipComponent, LegendComponent])
 </script>
